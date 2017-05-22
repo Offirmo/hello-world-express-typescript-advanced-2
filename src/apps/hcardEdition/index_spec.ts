3f60c7520d0ffcc18b23778c92ccf6cd5bcc5994
Offirmo <offirmo.net@gmail.com>
2017-05-23 00:17:49 +0800

    TLDW :scream_cat:

--- src/apps/hcardEdition/index_spec.ts
+++ src/apps/hcardEdition/index_spec.ts
@@ -1,7 +1,64 @@
 
+import * as request from 'supertest'
+
+import { User } from '../../models/user'
+import { CRUD } from '../../persistence/types'
+
+import { factory } from '.'
+
 
 describe('hCard edition app', function() {
 
-	it('works', () => console.log('it works'))
+	function makeUserCRUD(): CRUD<User> {
+		async function create(candidateData: Partial<User> = {}): Promise<string> {
+			throw new Error('Not implemented!')
 
+		}
+
+		async function read(userId: string): Promise<User | undefined> {
+			throw new Error('Not implemented!')
+
+		}
+
+		async function update(userId: string, candidateData: Partial<User>): Promise<void> {
+			throw new Error('Not implemented!')
+
+		}
+
+		async function purge(userId: string): Promise<void> {
+			throw new Error('Not implemented!')
+		}
+
+		return {
+			create,
+			read,
+			update,
+			purge,
+		}
+	}
+
+	describe('routes', function() {
+
+		describe('/', function() {
+
+			it('should serve an HTML page', (done) => {
+				factory({ userCRUD: makeUserCRUD() }).then(app => {
+					console.log(app)
+
+					request(app)
+					.get('/')
+					//.expect('Content-Type', /json/)
+					.expect(200, done)
+				})
+			})
+
+			it('should serve a form')
+		})
+
+		describe('/submit', function() {
+			it('accepts an urlencoded body containing from data')
+			it('should reject malformed data')
+		})
+
+	})
 })
